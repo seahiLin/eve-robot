92d75cfca0efec86d49e765cc1a9c40d47161deb
seahiLin <34095631+seahiLin@users.noreply.github.com>
2024-04-25 13:07:17 +0800

    test

--- pages/api/challenge.ts
+++ pages/api/challenge.ts
@@ -2,17 +2,24 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    // 获取POST请求中的参数
-    const { CHALLENGE } = req.body
+  console.log(req.method, req.body, 'req change')
+  const { CHALLENGE } = req.body
 
     
 
     // 返回响应以文本形式
-    res.status(200).send(CHALLENGE)
-    // res.status(200).json({ message: 'success', CHALLENGE })
-  } else {
-    // 如果不是POST请求，返回405 Method Not Allowed
-    res.status(405).json({ message: 'Method not allowed' })
-  }
+  res.status(200).send(CHALLENGE)
+  // if (req.method === 'POST') {
+  //   // 获取POST请求中的参数
+  //   const { CHALLENGE } = req.body
+
+    
+
+  //   // 返回响应以文本形式
+  //   res.status(200).send(CHALLENGE)
+  //   // res.status(200).json({ message: 'success', CHALLENGE })
+  // } else {
+  //   // 如果不是POST请求，返回405 Method Not Allowed
+  //   res.status(405).json({ message: 'Method not allowed' })
+  // }
 }
