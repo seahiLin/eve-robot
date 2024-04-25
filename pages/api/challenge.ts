79719f114377fe045ce6c3bff57f8f14822b1ded
seahiLin <34095631+seahiLin@users.noreply.github.com>
2024-04-25 16:54:58 +0800

    test

--- pages/api/challenge.ts
+++ pages/api/challenge.ts
@@ -3,12 +3,12 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.method, req.body, 'req change')
-  const { CHALLENGE } = req.body
+  const { challenge } = req.body
 
     
 
     // 返回响应以文本形式
-  res.status(200).send(CHALLENGE)
+  res.status(200).send(challenge)
   // if (req.method === 'POST') {
   //   // 获取POST请求中的参数
   //   const { CHALLENGE } = req.body
