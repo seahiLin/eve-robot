e36783bb82343db9504b838fff4a4cf58a7eced8
seahiLin <34095631+seahiLin@users.noreply.github.com>
2024-04-25 12:47:44 +0800

    feat

--- pages/api/challenge.ts
+++ pages/api/challenge.ts
@@ -8,8 +8,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     
 
-    // 返回响应
-    res.status(200).json({ message: 'success', CHALLENGE })
+    // 返回响应以文本形式
+    res.status(200).send(CHALLENGE)
+    // res.status(200).json({ message: 'success', CHALLENGE })
   } else {
     // 如果不是POST请求，返回405 Method Not Allowed
     res.status(405).json({ message: 'Method not allowed' })
