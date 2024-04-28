aa2889615ebd8e7196af1404a5dbfb3240a20a2d
seahiLin <34095631+seahiLin@users.noreply.github.com>
2024-04-28 10:20:55 +0800

    test

---
+++ lib/jira.ts
@@ -0,0 +1,10 @@
+import Jira from 'jira-client'
+
+export const jira = new Jira({
+  protocol: "https",
+  host: "jira.motiong.net",
+  username: "eve-robot",
+  password: "eve-robot",
+  apiVersion: "2",
+  strictSSL: true
+})
--- pages/api/challenge.ts
+++ pages/api/challenge.ts
@@ -1,4 +1,4 @@
-// pages/api/challenge.js
+import { jira } from "@/lib/jira";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -12,10 +12,29 @@ export default async function handler(
     res.status(200).json({ challenge });
     return;
   } else if (header.event_type === "card.action.trigger") {
-    const { value } = event.action
-    
+    const { value } = event.action;
+    const {
+      summary,
+      description,
+      assignee: { email },
+    } = JSON.parse(value);
 
-    
+    await jira.addNewIssue({
+      fields: {
+        project: {
+          key: "EV",
+        },
+        summary,
+        description,
+        assignee: {
+          name: email.split("@")[0],
+        },
+        issuetype: {
+          name: "Task",
+        },
+      },
+    });
+
     res.status(200).json({
       toast: {
         type: "success",
