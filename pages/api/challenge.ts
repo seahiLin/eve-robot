f89864e156c79ca99b89dea7a8fc67193c00769d
seahiLin <34095631+seahiLin@users.noreply.github.com>
2024-05-15 16:52:55 +0800

    test

--- pages/api/challenge.ts
+++ pages/api/challenge.ts
@@ -5,118 +5,125 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // const { challenge, event, header } = req.body;
-  const header = {
-    event_id: "8e1431a5d59f986398dff93ce89d458d",
-    token: "zPD1pEYf0mPqa8q77y96BbjlzXwScewk",
-    create_time: "1715685178220",
-    event_type: "im.message.receive_v1",
-    tenant_key: "1172885e3e4f975d",
-    app_id: "cli_a6b2ff48387a500d",
-  };
-  const event = {
-    message: {
-      chat_id: "oc_c2caec16ec5d62ebb3ee18f8dadc3da0",
-      chat_type: "p2p",
-      content: '{"text":"测试团队进展如何"}',
-      create_time: "1715685177961",
-      message_id: "om_787be71402e63428d812c71c4cd7e9f5",
-      message_type: "text",
-      update_time: "1715685177961",
-    },
-    sender: {
-      sender_id: {
-        open_id: "ou_870014ab6e3a0a73bca8fa560489c059",
-        union_id: "on_232001dbcb30eb6cf7f168a6299f7e3f",
-        user_id: "515e4a6b",
-      },
-      sender_type: "user",
-      tenant_key: "1172885e3e4f975d",
-    },
-  };
+  const { challenge, event, header } = req.body;
 
-  console.log(header, event, "header");
+  console.log("header, event, challenge", header, event, challenge);
 
-  if (header?.event_type === "im.message.receive_v1") {
+  if (challenge) {
+    res.status(200).json({ challenge });
+    return;
+  } else if (header?.event_type === "im.message.receive_v1") {
     const {
-      message: { chat_id, content },
+      message: { message_id, chat_type, content, mentions },
       sender: {
         sender_id: { user_id },
       },
     } = event;
 
-    handle(content, chat_id, user_id);
+    console.log(event, header, mentions, "param");
+
+    if (
+      chat_type === "p2p" ||
+      (chat_type === "group" &&
+        mentions?.find(
+          (mention: any) =>
+            mention?.id?.union_id === "on_501aec72fef5e5d2f38b3959840dd0ac" &&
+            mention?.id?.user_id === ""
+        ))
+    ) {
+      handleMessage(content, message_id, user_id);
+    }
 
     res.status(200).json({});
     return;
   }
 }
 
-const handle = async(content: any,chat_id: string, user_id: string) => {
+async function handleMessage(
+  content: any,
+  message_id: string,
+  user_id: string
+) {
   const jsonContent = JSON.parse(content);
+  let queryStr = "";
+  if (jsonContent.text) {
+    queryStr = jsonContent.text;
+  } else if (jsonContent.content?.length) {
+    const textValues = jsonContent.content.reduce(
+      (
+        acc: string[],
+        curr: {
+          tag: string;
+          text?: string;
+        }[]
+      ) => {
+        const textNodes = curr
+          .filter((item) => item.tag === "text")
+          .map((item) => item.text || "");
+        return acc.concat(textNodes);
+      },
+      []
+    );
+    queryStr = textValues.filter((t: string) => t).join(", ");
+  } else {
+    return;
+  }
 
-    let queryStr = "";
-    if (jsonContent.text) {
-      queryStr = jsonContent.text;
-    } else if (jsonContent.content?.length) {
-      const textValues = jsonContent.content.reduce(
-        (
-          acc: string[],
-          curr: {
-            tag: string;
-            text?: string;
-          }[]
-        ) => {
-          const textNodes = curr
-            .filter((item) => item.tag === "text")
-            .map((item) => item.text || "");
-          return acc.concat(textNodes);
-        },
-        []
-      );
-      queryStr = textValues.filter((t: string) => t).join(", ");
-    } else {
-      return;
-    }
-
-    console.log("queryStr: ", queryStr);
-
-    const ragResult = await fetch(
-      "https://api.rag.eve.platform.motiong.com/rag/v1/ask_query",
+  const result = {
+    text: "hello",
+    attachments: [
       {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          query: queryStr,
-        }),
+        file_key: "file_v3_00as_70b94316-7832-4ac1-8185-526c92bc881g",
+        file_name: "产品-柔性 1 (1) (1).png",
+      },
+      {
+        fil_key: "img_v3_02as_7e283f0e-aa06-43bf-b3d0-4fe6c526d36g",
       }
-    )
-      .then((res) => res.json())
-      .catch((err) => {
-        console.log("err: ", err);
-        return Promise.reject(err);
-      });
+    ],
+  }
 
-    if (ragResult.code === 0) {
-      const { tenant_access_token } = await requestTenantAccessToken();
-      fetch(
-        "https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=chat_id",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${tenant_access_token}`,
-          },
-          body: JSON.stringify({
-            receive_id: chat_id,
-            msg_type: "text",
-            content: `{\"text\":\"<at user_id=\\\"${user_id}\\\">you</at> ${ragResult.data.response}\"}`,
-          }),
+  const { tenant_access_token } = await requestTenantAccessToken();
+  fetch(`https://open.feishu.cn/open-apis/im/v1/messages/${message_id}/reply`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${tenant_access_token}`,
+    },
+    body: JSON.stringify({
+      msg_type: "post",
+      content: JSON.stringify({
+        zh_cn: {
+          content: [
+            [
+              {
+                tag: "text",
+                text: result.text,
+              },
+            ],
+            [
+              {
+                tag: "img",
+                image_key: result.attachments[1].file_key,
+              }
+            ]
+          ]
         }
-      );
-    }
+      })
+    }),
+  });
+  fetch(`https://open.feishu.cn/open-apis/im/v1/messages/${message_id}/reply`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${tenant_access_token}`,
+    },
+    body: JSON.stringify({
+      msg_type: "file",
+      content: JSON.stringify({
+        file_key: result.attachments[0].file_key,
+      })
+    }),
+  });
 }
 
 export function requestPreAuthorizationCode() {
--- test.js
+++ test.js
@@ -1,13 +1,75 @@
-const queryStr = "test下"
-fetch(
-  "https://api.rag.eve.platform.motiong.com/rag/v1/ask_query",
-  {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: queryStr,
-    }),
+
+const result = {
+  data: {
+    response: {
+      text: "hello",
+      attachments: [
+        {
+          file_key: "file_xxx",
+          file_name: "file_name",
+        },
+        {
+          file_key: "img_yyy",
+          file_name: "file_name",
+        }
+      ]
+    }
   }
-).then((res) => res.json()).then((data) => console.log(data));
+}
+
+async function dod() {
+  const { tenant_access_token } = await requestTenantAccessToken();
+  fetch(
+    "https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=chat_id",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${tenant_access_token}`,
+      },
+      body: JSON.stringify({
+        receive_id: chat_id,
+        msg_type: "text",
+        content: `{\"text\":\"<at user_id=\\\"${user_id}\\\">you</at> ${ragResult.data.response}\"}`,
+      }),
+    }
+  );
+}
+
+function sendFileMessage(fileKey, fileName) {
+  fetch(
+    "https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=chat_id",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${tenant_access_token}`,
+      },
+      body: JSON.stringify({
+        receive_id: chat_id,
+        msg_type: "file",
+        content: "{\"file_key\":\"file_v2_xxx\"}",
+      }),
+    }
+  );
+}
+
+async function requestTenantAccessToken() {
+  return fetch(
+    "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({
+        app_id: "cli_a6b2ff48387a500d",
+        app_secret: "MSMd1u4oj88TusihZAIJabQ1M4TylMsU",
+      }),
+    }
+  ).then((res) => {
+    return res.json();
+  });
+}
+
+dod()
